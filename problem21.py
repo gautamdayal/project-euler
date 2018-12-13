202854a0c4be1847bda6c245803e7f3da541237b
Gautam Dayal <gautam.dayal@yahoo.com>
2018-12-13 18:59:15 +0530

    Create problem21.py

---
+++ problem21.py
@@ -0,0 +1,14 @@
+def divSum(a):
+    return sum([i for i in range(1, a // 2 + 1) if a % i == 0])
+
+def isAmicable(a, b):
+    return divSum(a) == b and divSum(b) == a
+
+"""
+for a in range(1,10000):
+    for b in range(1,10000):
+        if isAmicable(a, b):
+            print(a, b)
+"""
+
+# ABOVE CODE IS WAYY TO SLOW. FIX.
