3f6a4d13002957596a8ef69988633c41204bec8c
Gautam Dayal <gautam.dayal@yahoo.com>
2018-12-16 12:38:59 +0530

    Create problem28.py

---
+++ problem28.py
@@ -0,0 +1,12 @@
+# Key to solving: top right number = square of ring number
+
+def diagonal(n):
+    sums = [1]
+    for i in range(1, n):
+        diagonalSum = (4 * (2*i + 1) ** 2) - 12 * i
+        sums.append(diagonalSum)
+    return sum(sums)
+
+print(diagonal(501))
+
+# Output: 669171001
