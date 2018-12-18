685d080525f66e447009b2c33f465ead352e42de
Gautam Dayal <gautam.dayal@yahoo.com>
2018-12-18 15:50:39 +0530

    Update problem02.py

--- problem02.py
+++ problem02.py
@@ -1,3 +1,11 @@
+def fibonacci(n):
+    n1, n2 = 0, 1
+    
+    for i in range(0, n):
+        n1, n2 = n2, n1 + n2
+        
+    return n1
+
 L = [fibonacci(i) for i in range(1, 35) if fibonacci(i) % 2 == 0]
 print(sum(L))
 
