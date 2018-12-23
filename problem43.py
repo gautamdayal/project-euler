7ed1e7c06838c810de2869294e7c93d0bbe3bde3
Gautam Dayal <gautam.dayal@yahoo.com>
2018-12-23 15:47:48 +0530

    Create problem43.py

---
+++ problem43.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+ 
+def splice(n):
+    s = str(n)
+    L = []
+    d, u = 1, 4
+    while u < 11:
+        L.append(int(s[d:u]))
+        d += 1
+        u += 1
+    return L
+
+def isCoolNum(n):
+    L = splice(n)
+    primes = [2, 3, 5, 7, 11, 13, 17]
+    c = 0
+    for i in range(len(L)):
+        if L[i] % primes[i] == 0:
+            c += 1
+    if c == 7:
+        return True
+    else:
+        return False
+
+L = list(itertools.permutations('1234567890'))
+pandigitals = [int(''.join(s)) for s in L]
+
+print(sum([n for n in pandigitals if isCoolNum(n)]))
+
+# Output: 16695334890
